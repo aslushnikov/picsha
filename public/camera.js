8abbe9e5972482eb9cd77333b79f9ab7dac35b15
Sedrak Kalashyan <inbox@sedrak.info>
2013-05-25 16:58:21 +0400

    logoc

--- public/camera.js
+++ public/camera.js
@@ -1,13 +1,20 @@
+var video = document.querySelector('video');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var localMediaStream = null;
 window.URL = window.URL || window.webkitURL;
+function turnon() {
 navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 if (navigator.getUserMedia) {
     navigator.getUserMedia({audio: false, video: true}, function(stream) {
         video.src = window.URL.createObjectURL(stream);
+        localMediaStream = stream;
     }, onFailSoHard);
 } else {
     video.src = 'somevideo.webm'; // fallback.
+}
 }
 
 // Not showing vendor prefixes or code that works cross-browser:
@@ -25,3 +32,17 @@ if (!navigator.getUserMedia) {
 } else {
     navigator.getUserMedia({video: true}, success, fallback);
 }
+var onFailSoHard = function(e) {
+    console.log('Reeeejected!', e);
+};
+
+
+function snapshot() {
+    if (localMediaStream) {
+        ctx.drawImage(video, 0, 0);
+        // "image/webp" works in Chrome 18. In other browsers, this will fall back to image/png.
+        document.querySelector('img').src = canvas.toDataURL('image/webp');
+    }
+}
+
+video.addEventListener('click', snapshot, false);
