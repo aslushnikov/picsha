0cf954b598c62d1b8168fa7f1eeee54456bbb0cb
Sedrak Kalashyan <inbox@sedrak.info>
2013-05-25 16:28:44 +0400

    now you can take your pic

--- public/camera.js
+++ public/camera.js
@@ -1,7 +1,27 @@
-/**
- * Created with JetBrains WebStorm.
- * User: mortigenus
- * Date: 25.05.13
- * Time: 12:40
- * To change this template use File | Settings | File Templates.
- */
+window.URL = window.URL || window.webkitURL;
+navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+if (navigator.getUserMedia) {
+    navigator.getUserMedia({audio: false, video: true}, function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+    }, onFailSoHard);
+} else {
+    video.src = 'somevideo.webm'; // fallback.
+}
+
+// Not showing vendor prefixes or code that works cross-browser:
+
+function fallback(e) {
+    video.src = 'fallbackvideo.webm';
+}
+
+function success(stream) {
+    video.src = window.URL.createObjectURL(stream);
+}
+
+if (!navigator.getUserMedia) {
+    fallback();
+} else {
+    navigator.getUserMedia({video: true}, success, fallback);
+}
