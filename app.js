a1c75385d4da9a6da7bc59ecc3bd794f37248bd9
Andrey Lushnikov <aslushnikov@gmail.com>
2013-05-25 17:58:05 +0400

    update formatting app.js

--- app.js
+++ app.js
@@ -11,36 +11,36 @@ var app = module.exports = express()
 var activeSockets = [];
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world, hello!' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-  socket.on('disconnect', function() {
-    var index = activeSockets.indexOf(socket);
-    if (index >= 0) activeSockets.splice(index, 1);
-  });
-  activeSockets.push(socket);
+    socket.emit('news', { hello: 'world, hello!' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+    socket.on('disconnect', function() {
+        var index = activeSockets.indexOf(socket);
+        if (index >= 0) activeSockets.splice(index, 1);
+    });
+    activeSockets.push(socket);
 });
 
 // Configuration
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.compress());
-  app.use(express.bodyParser());
-  app.use(express.cookieParser());
-  app.use(express.cookieSession({secret: "secreet"}));
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.compress());
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.cookieSession({secret: "secreet"}));
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 app.get("/", function (req, res) {
@@ -48,13 +48,13 @@ app.get("/", function (req, res) {
 });
 
 app.get("/clients", function (req, res) {
-  res.send('Active clients: ' + activeSockets.length);
+    res.send('Active clients: ' + activeSockets.length);
 });
 
 app.get('/session', function(req, res){
-  req.session.count = req.session.count || 0;
-  var n = req.session.count++;
-  res.send('viewed ' + n + ' times\n');
+    req.session.count = req.session.count || 0;
+    var n = req.session.count++;
+    res.send('viewed ' + n + ' times\n');
 });
 
 server.listen(process.env.PORT || 3000);
