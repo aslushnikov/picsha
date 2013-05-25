e0542f234b8dee1f951ebdcf30f3c666ad0c2c62
Ivan Chalov <mortigenus@gmail.com>
2013-05-25 21:47:48 +0400

    flip fixed

--- public/javascripts/photos.js
+++ public/javascripts/photos.js
@@ -1,3 +1,25 @@
+var photos = [];
+
+function Photo(id, src, lat, lon) {
+    this.id = id;
+    this.src = src;
+    this.lat = lat;
+    this.lon = lon;
+}
+
+function findPhotoById(id) {
+    for (var i = 0; i < photos.length; ++i) {
+        if (photos[i].id === id) {
+            return photos[i];
+        }
+    }
+    return null;
+}
+
+function addPhotoToModel(photo) {
+    photos.push(photo);
+}
+
 $(document).ready(function(){
     $('#button').click(function(){
         addPhotoToBottom(0, "http://i500.listal.com/image/3337547/500.jpg", 0, 0);
@@ -8,6 +30,7 @@ $(document).ready(function(){
 });
 
 function addPhoto(id, src, lat, lon, position) {
+    addPhotoToModel(new Photo(id, src,lat, lon));
     var photo = '<div class="photo" id="'+id+'" style="background:url('+src+');">' +
 //        '<img src="'+src+'" width=500px class="photo_img">' +
         '<div class="actions_bar">' +
@@ -21,7 +44,7 @@ function addPhoto(id, src, lat, lon, position) {
     } else if (position === "bottom") {
         $("#feed").append(photo);
     }
-    var $photoDiv = $("#"+id); $photoDiv._lat = lat; $photoDiv._lon = lon;
+    var $photoDiv = $("#"+id); $photoDiv.get(0)._lat = lat; $photoDiv.get(0)._lon = lon;
     $('.like').click(function(){
         if ($(this).attr('src') === "images/heart-no.png") {
             $(this).attr('src', "images/heart-yes.png");
@@ -32,12 +55,19 @@ function addPhoto(id, src, lat, lon, position) {
         //show map will be here
         var $img = $(this).parent().parent();
         $img.addClass('animated_flip');
+        $img.get(0)._onMap = !$img.get(0)._onMap;
         window.setTimeout(function () {
             return function () {
-                var src = "http://www.straitpinkie.com/wp-content/uploads/2011/12/04Kelly28-500x500.jpg";
+                var src = "";
+                if ($img.get(0)._onMap) {
+                    src = "http://www.straitpinkie.com/wp-content/uploads/2011/12/04Kelly28-500x500.jpg";
+                } else {
+                    src = findPhotoById(id).src;
+                }
+                console.log(src);
                 $img.css('background', 'url(' + src + ')');
             };
-        }, 500);
+        }(), 500);
 
         $img.on('transitionend webkitTransitionEnd MSTransitionEnd oTransitionEnd', function() {
             $img.removeClass('animated_flip');
