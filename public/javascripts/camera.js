4773477f069381036236ebb83fd3c3d10e2001b9
Sedrak Kalashyan <inbox@sedrak.info>
2013-05-25 20:30:09 +0400

    new webcam reader

--- public/javascripts/camera.js
+++ public/javascripts/camera.js
@@ -6,7 +6,6 @@ window.URL = window.URL || window.webkitURL;
 function turnon() {
 navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
 if (navigator.getUserMedia) {
     navigator.getUserMedia({audio: false, video: true}, function(stream) {
         video.src = window.URL.createObjectURL(stream);
@@ -15,6 +14,8 @@ if (navigator.getUserMedia) {
 } else {
     video.src = 'somevideo.webm'; // fallback.
 }
+    document.getElementById("take").onclick = snapshot;
+    document.getElementById('take').innerHTML = "Take a Picture";
 }
 
 // Not showing vendor prefixes or code that works cross-browser:
@@ -41,8 +42,24 @@ function snapshot() {
     if (localMediaStream) {
         ctx.drawImage(video, 0, 0);
         // "image/webp" works in Chrome 18. In other browsers, this will fall back to image/png.
-        document.querySelector('img').src = canvas.toDataURL('image/webp');
+        var img = document.querySelector('img');
+        img.src = canvas.toDataURL('image/webp');
+        img.style.position = "absolute";
+        img.style.width = video.offsetWidth + "px";
+        img.style.height = video.offsetHeight + "px";
+        img.style.left = video.offsetLeft + "px";
+        img.style.top = video.offsetTop + "px";
     }
+    document.getElementById("pic").style.display = "block";
+    document.getElementById("take").onclick = retake;
+    document.getElementById('take').innerHTML = "Retake";
+    document.getElementById("use").style.display = "inline";
+
+}
+function retake() {
+    document.getElementById("pic").style.display = "none";
+    document.getElementById("take").onclick = snapshot;
+    document.getElementById('take').innerHTML = "Take a Picture";
 }
 
 video.addEventListener('click', snapshot, false);
