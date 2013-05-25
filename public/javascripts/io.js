0755a1e404a31012f04292941c0c1e3b2f3ae711
Andrey Lushnikov <aslushnikov@gmail.com>
2013-05-25 16:42:40 +0400

    fix wrong url for socket.io

--- public/javascripts/io.js
+++ public/javascripts/io.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var socket = io.connect('http://localhost:3000');
+    var socket = io.connect('/');
     socket.on('news', function (data) {
         console.log(data);
         socket.emit("my other event", "bitch!");
