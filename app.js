948ee3b504f9bdad64e59030791d63fed3b284b9
Andrey Lushnikov <aslushnikov@gmail.com>
2013-05-25 11:42:49 +0400

    index.html example

--- app.js
+++ app.js
@@ -29,7 +29,5 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
-
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
