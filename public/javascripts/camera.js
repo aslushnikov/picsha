42abdacdd0e10946751b61729be1e9a0b1f42572
Andrey Lushnikov <aslushnikov@gmail.com>
2013-05-28 00:50:40 +0400

    Actual mobile support

--- public/javascripts/camera.js
+++ public/javascripts/camera.js
@@ -1,9 +1,31 @@
-var video, canvas, ctx, cropCanvas;
+var video, canvas, ctx, cropCanvas = document.createElement("canvas"),
+    mobileImageUpload = document.createElement("img");
 $(document).ready(function () {
     video = document.querySelector('video');
     canvas = document.querySelector('canvas');
     turnon();
+    $("#mobile-camera-fallback").on("change", mobileUpload);
 });
+
+function mobileUpload() {
+    var oFReader = new FileReader(), rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
+    if (document.getElementById("mobile-camera-fallback").files.length === 0) {
+        return;
+    }
+    var oFile = document.getElementById("mobile-camera-fallback").files[0];
+    if (!rFilter.test(oFile.type)) {
+        alert("You must select a valid image file!"); return;
+    }
+    oFReader.onload = function (oFREvent) {
+        var base64url = oFREvent.target.result;
+        mobileImageUpload.onload = function() {
+            cropImageSource(mobileImageUpload);
+            usePhoto();
+        }
+        mobileImageUpload.src = base64url;
+    }
+    oFReader.readAsDataURL(oFile);
+}
 
 var localMediaStream = null;
 window.URL = window.URL || window.webkitURL;
@@ -55,6 +77,15 @@ var onFailSoHard = function (e) {
     console.log('Reeeejected!', e);
 };
 
+function cropImageSource(target) {
+    // crop image to make in rectangular
+    var size = Math.min(target.width, target.height);
+    cropCanvas.width = size;
+    cropCanvas.height = size;
+    var cropCtx = cropCanvas.getContext("2d");
+    cropCtx.drawImage(target, (size - target.width) / 2, (size - target.height) / 2, target.width, target.height);
+}
+
 function snapshot() {
     if (localMediaStream) {
         var img = $("#camera img").get(0);
@@ -66,14 +97,7 @@ function snapshot() {
         canvas.style.left = video.offsetLeft + "px";
         canvas.style.top = video.offsetTop + "px";
         $(canvas).show();
-
-        // crop image to make in rectangular
-        cropCanvas = document.createElement("canvas");
-        var size = Math.min(canvas.width, canvas.height);
-        cropCanvas.width = size;
-        cropCanvas.height = size;
-        var cropCtx = cropCanvas.getContext("2d");
-        cropCtx.drawImage(video, (size - canvas.width) / 2, (size - canvas.height) / 2, canvas.width, canvas.height);
+        cropImageSource(canvas);
     }
     $("#snapshot-button").hide();
     $("#use-button").show();
